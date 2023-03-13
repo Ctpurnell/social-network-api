3676c973fd08c863a49679a87bc484478db631aa
Christopher Purnell <112015234+Ctpurnell@users.noreply.github.com>
2023-03-12 21:12:53 -0400

    listening

--- config/connection.js
+++ config/connection.js
@@ -1,11 +1,20 @@
-const { connect, connection } = require('mongoose');
+const express = require('express');
+const mongodb = require('mongodb').MongoClient;
 
-const connectionString =
-  process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/socialDB';
+const app = express();
+const port = 3001;
 
-connect(connectionString, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
+const connectionStringURI = `mongodb://127.0.0.1:27017/socialDB`;
 
-module.exports = connection;
+let db;
+
+mongodb.connect(
+  connectionStringURI,
+  { useNewUrlParser: true,useUnifiedTopology: true },
+  (err, client) => {
+    db = client.db();
+    app.listen(port, () => {
+      console.log(`Example app listening at http://localhost:${port}`);
+    });
+  }
+);
--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const db = require('./config/connection');
-const routes = require('./routes');
+
 
 
 const PORT = process.env.PORT || 3001;
@@ -12,10 +12,5 @@ const app = express();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(routes);
 
-db.once('open', () => {
-  app.listen(PORT, () => {
-    console.log(`API server for ${activity} running on port ${PORT}!`);
-  });
-});
+
